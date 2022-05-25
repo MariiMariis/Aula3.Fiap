fd1a12b6754f0ed113158441fa62ab7956437108
MariiMariis <mariana.motta94@gmail.com>
2022-05-25 20:21:55 -0300

    Add project files.

---
+++ Aula3.Fiap/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula3.Fiap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
