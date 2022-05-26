e87568e7be69f2daa7330fb4c901f27829dc43bb
MariiMariis <mariana.motta94@gmail.com>
2022-05-25 21:44:53 -0300

    Aula 3

---
+++ Aula3.Fiap/Models/Casa.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula3.Fiap.Models
+{
+    class Casa
+    {
+
+        //Propriedades
+        public int Comodos { get; set; }
+        public bool Piscina { get; set; }
+        public double AreaTotal { get; set; }
+
+        //Contrutores
+
+        public Casa(int comodos, bool piscina, double areaTotal)
+        {
+            Comodos = comodos;
+            Piscina = piscina;
+            AreaTotal = areaTotal;
+        }
+
+        //Construtor que recebe apenas a àrea
+        public Casa(double areaTotal)
+        {
+            AreaTotal = areaTotal;  
+        }
+
+        //Construtor sem parâmetros
+        public Casa() { }
+        
+    }
+}
--- Aula3.Fiap/Program.cs
+++ Aula3.Fiap/Program.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using Aula3.Fiap.Models;
+using System;
 
 namespace Aula3.Fiap
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");dd
+            //Ler o input do usuário
+
+            Console.WriteLine("Digite a quantidade de cômodos:");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Tem piscina?(true/false)");
+            bool piscina = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a àrea da casa:");
+            var area = double.Parse(Console.ReadLine());
+
+            //Utilizando atributos informados no construtor, com input do usuário
+            var casa = new Casa(quantidade, piscina, area);
+
+            // Construtor utilizando parâmetros pré definidos
+            var casa2 = new Casa(6, true, 123);
+
+            //Construtor sem parâmetros
+            var casa3 = new Casa()
+            {
+                Comodos = 5,
+                AreaTotal = 50
+            };
+
+            //Construtor com 1 parâmetro
+            var casa4 = new Casa(70)
+            {
+                Piscina = true
+            };
+
+            Console.WriteLine($"{casa.Comodos}, {casa.Piscina}. {casa.AreaTotal}");
+            Console.WriteLine($"{casa2.Comodos}, {casa2.Piscina}. {casa2.AreaTotal}");
+            Console.WriteLine($"{casa3.Comodos}, {casa3.Piscina}. {casa3.AreaTotal}");
+            Console.WriteLine($"{casa4.Comodos}, {casa4.Piscina}. {casa4.AreaTotal}");
+
         }
     }
 }
